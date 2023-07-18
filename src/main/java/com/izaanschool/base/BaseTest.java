f43a57414f8da8d48c295a16172ac0530a295b93
AyshaShetu <ashetu.ctg@gmail.com>
2023-07-17 20:24:11 -0500

    2nd commit trying to fix SmokeTestSuit

--- src/main/java/com/izaanschool/base/BaseTest.java
+++ src/main/java/com/izaanschool/base/BaseTest.java
@@ -39,7 +39,7 @@ public class BaseTest {
 
 		// This sleep here is for instructor only. Students don't need this here
 		try {
-			Thread.sleep(5000);
+			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
